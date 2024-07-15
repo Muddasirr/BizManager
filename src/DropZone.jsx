35b057c99c356f8c6ea9fd161a3bbd88a1ae0d9b
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-07-15 14:09:44 +0500

    Update DropZone.jsx

--- src/DropZone.jsx
+++ src/DropZone.jsx
@@ -1,31 +1,40 @@
 import React, { useState } from 'react';
 import { useDrop } from 'react-dnd';
+import { Box, Typography, TableCell } from '@mui/material';
+import { hex } from 'chroma-js';
 
-const DropZone = ({onDrop}) => {
-  const [value, setValue] = useState('');
+const DropZone = ({ onDrop, name }) => {
+  const [value, setValue] = useState(name);
+
   const [{ isOver }, drop] = useDrop(() => ({
     accept: 'item',
     drop: (item) => {
 
-    onDrop(item);
+      onDrop(item);
       setValue(item.name);
-      
-      
+
+
     },
     collect: (monitor) => ({
       isOver: monitor.isOver(),
     }),
   }));
 
-
+  const style = (value) => ({
+    backgroundColor: value === "" ? 'white' : '#cceeff',
+    borderRadius: 1,
+    textAlign: 'center'});
 
   return (
-    <div
-      ref={drop}
-     >
-    <input type="text" value={value} style={{background:'#cceeff',border:'none', width:"200px"}} />
-      
-    </div>
+
+
+    <Box ref={drop} sx={
+
+
+      style(value)}>
+      <TableCell >{value}</TableCell>
+    </Box>
+
   );
 };
 
