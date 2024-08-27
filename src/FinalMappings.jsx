45d5c7d238aaa8049444ff0e4505f4ae6fc76c18
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-08-27 11:28:46 +0500

    commit

--- src/FinalMappings.jsx
+++ src/FinalMappings.jsx
@@ -10,9 +10,42 @@ import {
     TableRow,
     Paper,
     Pagination,
-    Button
+    Button,
+    PaginationItem
 } from '@mui/material';
 import Navbar from './NavBar';
+import { styled } from '@mui/system';
+
+const CustomPagination = styled(Pagination)({
+    '& .MuiPagination-ul': {
+        justifyContent: 'center',
+    },
+    '& .MuiPaginationItem-root': {
+        borderRadius: '24px', 
+        margin: '0 4px', 
+        padding: '6px 12px', 
+    },
+    '& .MuiPaginationItem-page': {
+        backgroundColor: '#E7E8F4',
+        color: '#2F39B6', 
+        '&.Mui-selected': {
+            backgroundColor: '#2F39B6',
+            color: '#fff', 
+        },
+        '&:hover': {
+            backgroundColor: '#2F39B6',
+            color: '#fff',
+        }
+    },
+    '& .MuiPaginationItem-previousNext': {
+        backgroundColor: '#E7E8F4', // Background color for Previous/Next buttons
+        color: '#2F39B6', // Text color for Previous/Next buttons
+        '&:hover': {
+            backgroundColor: '#2F39B6',
+            color: '#fff',
+        }
+    }
+});
 
 const ValidationFinalMappings = () => {
     const earningAndMemosData = [
@@ -21,112 +54,104 @@ const ValidationFinalMappings = () => {
         { extractedHeader: 'taxes_medical_care', finalMappedHours: 'taxes_medical_care' },
         { extractedHeader: 'taxes_state_income', finalMappedHours: 'taxes_state_income' },
         { extractedHeader: 'taxes_state_special_pa8053_city', finalMappedHours: 'taxes_state_special_pa8053_city' },
-       
     ];
 
     const deductionsData = [
         { extractedHeader: 'deduction_retirement_401k', finalMappedHours: 'D_401k' },
         { extractedHeader: 'deduction_retirement_401k', finalMappedHours: 'D_401k' },
-       
     ];
 
     const taxesData = [
         { extractedHeader: 'taxes_fed_income', finalMappedHours: 'T_FEDWIT' },
         { extractedHeader: 'taxes_social_security', finalMappedHours: 'T_SOCSCEE' },
-      
     ];
 
     return (
         <Box>
-            <Navbar/>
-        <Box sx={{ padding: '24px' }}>
-            <Typography variant="h6" fontWeight="bold">Validation - <Typography component="span" sx={{ color: '#2F39B6' }}>Final Mappings</Typography></Typography>
-            <Typography variant="body2" color="textSecondary" gutterBottom>
-                Validate the Final Header Mappings for all the sections and click approve to generate your excel file.
-            </Typography>
+            <Navbar />
+            <Box sx={{ padding: '24px' }}>
+                <Typography variant="h6" fontWeight="bold">Validation - <Typography component="span" fontWeight='600' sx={{ color: '#2F39B6' }}>Final Mappings</Typography></Typography>
+                <Typography variant="body2" color="textSecondary" gutterBottom>
+                    Validate the Final Header Mappings for all the sections and click approve to generate your excel file.
+                </Typography>
 
-            
-            <Box sx={{ marginBottom: '24px' }}>
-                <TableContainer component={Paper}>
-                    <Typography variant="subtitle1" fontWeight="bold" sx={{ padding: '16px' }}>Earning And Memos</Typography>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Extracted Header</TableCell>
-                                <TableCell>Final Mapped Hours</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {earningAndMemosData.map((row, idx) => (
-                                <TableRow key={idx}>
-                                    <TableCell>{row.extractedHeader}</TableCell>
-                                    <TableCell>{row.finalMappedHours}</TableCell>
+                <Box sx={{ marginBottom: '24px' }}>
+                    <TableContainer component={Paper}>
+                        <Typography variant="subtitle1" fontWeight="bold" sx={{ padding: '16px' }}>Earning And Memos</Typography>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Extracted Header</TableCell>
+                                    <TableCell>Final Mapped Hours</TableCell>
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <Box justifyContent='center' alignItems='center' width='100%'>
-                <Pagination  count={6} color="primary" />
+                            </TableHead>
+                            <TableBody>
+                                {earningAndMemosData.map((row, idx) => (
+                                    <TableRow key={idx}>
+                                        <TableCell>{row.extractedHeader}</TableCell>
+                                        <TableCell>{row.finalMappedHours}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <Box mt={2}>
+                    <CustomPagination count={6} color="primary" />
+                    </Box>
+                </Box>
+
+                <Box sx={{ marginBottom: '24px' }}>
+                    <TableContainer component={Paper}>
+                        <Typography variant="subtitle1" fontWeight="bold" sx={{ padding: '16px' }}>Deductions</Typography>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Extracted Header</TableCell>
+                                    <TableCell>Final Mapped Hours</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {deductionsData.map((row, idx) => (
+                                    <TableRow key={idx}>
+                                        <TableCell>{row.extractedHeader}</TableCell>
+                                        <TableCell>{row.finalMappedHours}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
+
+                <Box sx={{ marginBottom: '24px' }}>
+                    <TableContainer component={Paper}>
+                        <Typography variant="subtitle1" fontWeight="bold" sx={{ padding: '16px' }}>Taxes</Typography>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Extracted Header</TableCell>
+                                    <TableCell>Final Mapped Hours</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {taxesData.map((row, idx) => (
+                                    <TableRow key={idx}>
+                                        <TableCell>{row.extractedHeader}</TableCell>
+                                        <TableCell>{row.finalMappedHours}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Box>
+
+                <Box display="flex" justifyContent="flex-end" alignItems="center" marginY={2}>
+                    <Box>
+                        <Button variant="contained" sx={{ marginRight: '8px', background: 'black', borderRadius: '24px', textTransform: 'none' }}>Generate Excel</Button>
+                        <Button variant="outlined" sx={{ marginRight: '8px', background: '#E7E8F4', borderRadius: '24px', textTransform: 'none' }}>Go Back to Edit</Button>
+                        <Button variant="contained" sx={{ background: '#2F39B6', borderRadius: '24px', textTransform: 'none' }}>Approve</Button>
+                    </Box>
                 </Box>
             </Box>
-
-          
-            <Box sx={{ marginBottom: '24px' }}>
-                <TableContainer component={Paper}>
-                    <Typography variant="subtitle1" fontWeight="bold" sx={{ padding: '16px' }}>Deductions</Typography>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Extracted Header</TableCell>
-                                <TableCell>Final Mapped Hours</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {deductionsData.map((row, idx) => (
-                                <TableRow key={idx}>
-                                    <TableCell>{row.extractedHeader}</TableCell>
-                                    <TableCell>{row.finalMappedHours}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Box>
-
-            
-            <Box sx={{ marginBottom: '24px' }}>
-                <TableContainer component={Paper}>
-                    <Typography variant="subtitle1" fontWeight="bold" sx={{ padding: '16px' }}>Taxes</Typography>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Extracted Header</TableCell>
-                                <TableCell>Final Mapped Hours</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {taxesData.map((row, idx) => (
-                                <TableRow key={idx}>
-                                    <TableCell>{row.extractedHeader}</TableCell>
-                                    <TableCell>{row.finalMappedHours}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Box>
-
-            
-            <Box display="flex" justifyContent="flex-end" alignItems="center" marginY={2}>
-               
-                <Box>
-                    <Button variant="contained" sx={{ marginRight: '8px',background:'black',borderRadius:'8px' }}>Generate Excel</Button>
-                    <Button variant="outlined" sx={{ marginRight: '8px',background:'#E7E8F4',borderRadius:'8px' }}>Go Back to Edit</Button>
-                    <Button variant="contained" sx={{background:'#2F39B6',borderRadius:'8px'}}>Approve</Button>
-                </Box>
-            </Box>
-        </Box>
         </Box>
     );
 };
